4618cfc5107c2e94c23143abe8ced813b05d6eff
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2021-12-31 11:02:38 +0200

    added gulp-files-include

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -8,12 +8,15 @@ const srcFolder = './src';
 
 export const path = {
     build: {
+        html: `${buildFolder}/`,
         files: `${buildFolder}/files/`
     },
     src: {
+        html: `${srcFolder}/*.html`,
         files: `${srcFolder}/files/**/*.*`
     },
     watch: {
+        html: `${srcFolder}/**/*.html`,
         files: `${srcFolder}/files/**/*.*`
     },
     clean: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ import gulp from "gulp";
 //Importing path
 import { path } from "./gulp/config/path.js"
 import { reset } from "./gulp/tasks/reset.js"
+import { html } from "./gulp/tasks/html.js"
 
 global.app = {
     path: path,
@@ -20,9 +21,11 @@ import { copy } from "./gulp/tasks/copy.js";
 //Watcher Function
 function watcher() {
     gulp.watch(path.watch.files, copy)
+    gulp.watch(path.watch.html, html)
 }
 
+const mainTasks = gulp.parallel(copy, html)
 //dev tasks
-const dev = gulp.series(reset, copy, watcher)
+const dev = gulp.series(reset, mainTasks, watcher)
 
 gulp.task('default', dev);
